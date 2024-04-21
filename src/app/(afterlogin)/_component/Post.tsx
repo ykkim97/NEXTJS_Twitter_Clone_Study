ba3ff4d77542e0535d5e3b52bfa1acad8ceb2fbd
ykkim97 <97ykkim@naver.com>
2024-04-22 01:06:24 +0900

    Complete FrontEnd

--- src/app/(afterlogin)/_component/Post.tsx
+++ src/app/(afterlogin)/_component/Post.tsx
@@ -46,28 +46,28 @@ export default function Post({ noImage } : Props) {
         <PostArticle post={target}>
             <div className={styles.postWrapper}>
                 <div className={styles.postUserSection}>
-                    <Link href={`/${target.User.id}`} className={styles.postUserImage}>
-                        <img src={target.User.image} alt={target.User.nickname}/>
-                        <div className={styles.postShade} />
-                    </Link>
+                <Link href={`/${target.User.id}`} className={styles.postUserImage}>
+                    <img src={target.User.image} alt={target.User.nickname}/>
+                    <div className={styles.postShade}/>
+                </Link>
                 </div>
                 <div className={styles.postBody}>
-                    <div className={styles.postMeta}>
-                        <Link href={`/${target.User.id}`}>
-                        <span className={styles.postUserName}>{target.User.nickname}</span>
-                        &nbsp;
-                        <span className={styles.postUserId}>@{target.User.id}</span>
-                        &nbsp;
-                        ·
-                        &nbsp;
-                        </Link>
-                        <span className={styles.postDate}>{dayjs(target.createdAt).fromNow(true)}</span>
-                    </div>
-                    <div>{target.content}</div>
-                    <div>
-                        <PostImages post={target}/>
-                    </div>
-                    <ActionButtons />
+                <div className={styles.postMeta}>
+                    <Link href={`/${target.User.id}`}>
+                    <span className={styles.postUserName}>{target.User.nickname}</span>
+                    &nbsp;
+                    <span className={styles.postUserId}>@{target.User.id}</span>
+                    &nbsp;
+                    ·
+                    &nbsp;
+                    </Link>
+                    <span className={styles.postDate}>{dayjs(target.createdAt).fromNow(true)}</span>
+                </div>
+                <div>{target.content}</div>
+                <div>
+                    <PostImages post={target} />
+                </div>
+                <ActionButtons/>
                 </div>
             </div>
         </PostArticle>
